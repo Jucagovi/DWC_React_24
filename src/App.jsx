0d3f30bc8fd435864ddec1845410cbab38e5b0af
Jucagovi <jc.gomez@edu.gva.es>
2024-10-17 16:46:55 +0200

    UT03 Quitar componente <NUmerocis2>.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import Clases from "./componentes/UT03/useRef/Clases.jsx";
 import Listado from "./componentes/UT03/useRef/Listado.jsx";
-import NUmerocis2 from "./componentes/solucion_301/NUmerocis2.jsx";
 import Numericos from "./componentes/solucion_301/Numericos.jsx";
 
 function App() {
   return (
     <>
       <h1>¡Bienvenido/a a React!.</h1>
-      <NUmerocis2 />
+      <Numericos />
     </>
   );
 }
