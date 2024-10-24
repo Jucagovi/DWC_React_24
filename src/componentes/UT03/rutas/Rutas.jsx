75f61f985853067555bde7c50bdb9026df06e9a7
Jucagovi <jc.gomez@edu.gva.es>
2024-10-24 16:50:27 +0200

    UT03 Cambios en clase submenús

--- src/componentes/UT03/rutas/Rutas.jsx
+++ src/componentes/UT03/rutas/Rutas.jsx
@@ -8,6 +8,10 @@ import Error from "./Error.jsx";
 import Administracion from "./Administracion.jsx";
 import listado from "../../../Practica 2.08/objetos/peliculas.json";
 import PeliculaDetalle from "./parametros/PeliculaDetalle.jsx";
+import AdminPeliculas from "./submenu/AdminPeliculas.jsx";
+import AdminInterpretes from "./submenu/AdminInterpretes.jsx";
+import AdminUsuarios from "./submenu/AdminUsuarios.jsx";
+import PeliculasListado from "./parametros/PeliculasListado.jsx";
 
 const Rutas = () => {
   return (
@@ -18,10 +22,14 @@ const Rutas = () => {
         <Route path='/acerca-de' element={<AcercaDe />} />
         <Route
           path='/peliculas'
-          element={<Peliculas listado={listado.peliculas} />}
+          element={<PeliculasListado listado={listado.peliculas} />}
         />
         {/* Creación de submenús. */}
-        <Route path='/administracion' element={<Administracion />} />
+        <Route path='/administracion' element={<Administracion />}>
+          <Route path='peliculas' element={<AdminPeliculas />} />
+          <Route path='interpretes' element={<AdminInterpretes />} />
+          <Route path='usuarios' element={<AdminUsuarios />} />
+        </Route>
         {/* Uso de parámetros en las rutas. */}
         <Route
           path='/pelicula-detalle/:identificador'
